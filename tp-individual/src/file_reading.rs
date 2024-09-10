4579f48a91b77ecd37e61e071ae8fa7ef05ace9e
MaximoGismondi <maximogismondi@gmail.com>
2024-09-10 12:22:44 -0300

    small corrections

--- tp-individual/src/file_reading.rs
+++ tp-individual/src/file_reading.rs
@@ -1,10 +1,10 @@
+use rayon::prelude::*;
+
 use std::{
     fs::File,
     io::{BufRead, BufReader},
     path::{Path, PathBuf},
 };
-
-use rayon::prelude::*;
 
 const CSV_EXTENSION: &str = "csv";
 
@@ -12,8 +12,7 @@ pub fn find_csv_in_dir(input_path: &str) -> Vec<PathBuf> {
     match std::fs::read_dir(input_path) {
         Ok(files) => files
             .filter_map(|file| {
-                let file = file.ok()?;
-                let path = file.path();
+                let path = file.ok()?.path();
                 if path.extension()?.to_str()? == CSV_EXTENSION {
                     Some(path)
                 } else {
@@ -38,17 +37,17 @@ fn read_csv_file(file: &Path) -> BufReader<File> {
     }
 }
 
-pub fn read_csv_files<T, K>(files: Vec<PathBuf>, process_line: T) -> impl ParallelIterator<Item = K>
+pub fn read_csv_files<F, T>(files: Vec<PathBuf>, process_line: F) -> impl ParallelIterator<Item = T>
 where
-    T: Fn(String) -> Result<K, String> + Send + Sync + 'static,
-    K: Send + 'static,
+    F: Fn(String) -> Result<T, String> + Send + Sync,
+    T: Send,
 {
     files
-        .into_par_iter() // Consumes the Vec<PathBuf>
+        .into_par_iter()
         .flat_map(|file| {
-            let reader = read_csv_file(&file); // Borrow file instead of consuming
-            reader.lines().skip(1).par_bridge() // Use par_bridge for parallelism
+            let reader = read_csv_file(&file);
+            reader.lines().skip(1).par_bridge()
         })
-        .filter_map(|line| line.ok()) // Filter out errors from reading lines
-        .filter_map(move |line| process_line(line).ok()) // Filter out errors from processing lines
+        .filter_map(|line| line.ok())
+        .filter_map(move |line| process_line(line).ok())
 }
