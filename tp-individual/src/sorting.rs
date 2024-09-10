5a2a46708aff4b0aecfad8d8dc393c744c33887b
MaximoGismondi <maximogismondi@gmail.com>
2024-09-10 12:06:24 -0300

    custom struct

--- tp-individual/src/sorting.rs
+++ tp-individual/src/sorting.rs
@@ -5,6 +5,44 @@ use std::{
     hash::Hash,
 };
 
+struct CappedMinHeapMap<K, V> {
+    heap: BinaryHeap<(Reverse<V>, K)>,
+    capacity: usize,
+}
+
+impl<K, V> CappedMinHeapMap<K, V>
+where
+    K: Ord,
+    V: Ord,
+{
+    fn new(capacity: usize) -> Self {
+        Self {
+            heap: BinaryHeap::new(),
+            capacity,
+        }
+    }
+
+    fn push(&mut self, key: K, value: V) {
+        self.heap.push((Reverse(value), key));
+
+        if self.heap.len() > self.capacity {
+            self.heap.pop();
+        }
+    }
+
+    fn into_iter(self) -> impl Iterator<Item = (K, V)> {
+        self.heap
+            .into_iter()
+            .map(|(Reverse(value), key)| (key, value))
+    }
+
+    fn merge(&mut self, other: Self) {
+        for (key, value) in other.into_iter() {
+            self.push(key, value);
+        }
+    }
+}
+
 pub fn retain_top_elements<K, V>(elements: &mut HashMap<K, V>, top_count: usize)
 where
     K: Hash + Ord + Send,
@@ -13,28 +51,20 @@ where
     let top_elements = elements
         .drain()
         .par_bridge()
-        .fold(BinaryHeap::new, |mut acc_heap, (key, value)| {
-            acc_heap.push((Reverse(value), key));
+        .fold(
+            || CappedMinHeapMap::new(top_count),
+            |mut acc_heap, (key, value)| {
+                acc_heap.push(key, value);
+                acc_heap
+            },
+        )
+        .reduce(
+            || CappedMinHeapMap::new(top_count),
+            |mut acc_heap, local_heap| {
+                acc_heap.merge(local_heap);
+                acc_heap
+            },
+        );
 
-            if acc_heap.len() > top_count {
-                acc_heap.pop();
-            }
-            acc_heap
-        })
-        .reduce(BinaryHeap::new, |mut acc_heap, local_heap| {
-            for (value, key) in local_heap.into_iter() {
-                acc_heap.push((value, key));
-
-                if acc_heap.len() > top_count {
-                    acc_heap.pop();
-                }
-            }
-            acc_heap
-        });
-
-    elements.extend(
-        top_elements
-            .into_iter()
-            .map(|(Reverse(value), key)| (key, value)),
-    );
+    elements.extend(top_elements.into_iter());
 }
